19590200ba624758770a8bba87b1153b70fe6508
Scott <scottleedevelopment@gmail.com>
2025-01-14 10:55:06 -0600

    corrected DataTypes code

--- DataTypes.cpp
+++ DataTypes.cpp
@@ -7,43 +7,48 @@
 // variables.  What can you learn about the different data types?
 //
 
-
 #include <cstdlib>
 #include <iostream>
 using namespace std;
 
 int main()
 {
-	 classAverage = 90.7; //Decimal number
-	 letterScore = 'A'; //Single letter
-	 testScore = 95; //Whole number value
-	 classTestAverage = 88.4f; //Decimal number, notice the 'f' at the end
-	 colorCode{
-		Green = 1,
-		Yellow = 5,
-		Red = 10
-	} gradebookColor; //Stores list of values
-	gradebookColor = Green; //This line does not need a declaration, it was declared in the line above
-	 isStudentPassing = true; //Could be true or false
+    // Declare variables with the appropriate data types
+    double classAverage = 90.7; // Decimal number
+    char letterScore = 'A'; // Single letter
+    int testScore = 95; // Whole number value
+    float classTestAverage = 88.4f; // Decimal number, notice the 'f' at the end
 
-	cout << "The class average is currently "
-		<< classAverage
-		<< endl;
-	cout << "The class test average was "
-		<< classTestAverage
-		<< endl;
-	cout << "Your test score was "
-		<< testScore
-		<< endl;
-	cout << "Your current letter score is "
-		<< letterScore
-		<< endl;
-	cout << "The color of your gradebook entry is "
-		<< gradebookColor
-		<< endl;
-	cout << "Are you passing? "
-		<< boolalpha		//This line allows the word 'true' or 'false' to be printed instead of '0' or '1'
-		<< isStudentPassing
-		<< endl;
-	return 0;
+    enum colorCode {
+        Green = 1,
+        Yellow = 5,
+        Red = 10
+    }; 
+    colorCode gradebookColor; // Stores list of values
+    gradebookColor = Green; // This line assigns the value Green
+
+    bool isStudentPassing = true; // Could be true or false
+
+    // Output the values of the variables
+    cout << "The class average is currently "
+         << classAverage
+         << endl;
+    cout << "The class test average was "
+         << classTestAverage
+         << endl;
+    cout << "Your test score was "
+         << testScore
+         << endl;
+    cout << "Your current letter score is "
+         << letterScore
+         << endl;
+    cout << "The color of your gradebook entry is "
+         << gradebookColor
+         << endl;
+    cout << "Are you passing? "
+         << boolalpha    // This line allows the word 'true' or 'false' to be printed instead of '0' or '1'
+         << isStudentPassing
+         << endl;
+
+    return 0;
 }
